de78de2ac100bf209c16434a0256a435553269b1
xiangbai <wanglei8568@126.com>
2014-05-06 20:10:47 +0800

    单例类，创建一个字典类只能被初始化一次

---
+++ include/Dictionary.h
@@ -0,0 +1,46 @@
+/*
+ * Dictionary.h
+ *
+ *  Created on: May 6, 2014
+ *      Author: wang
+ */
+
+#ifndef DICTIONARY_H_
+#define DICTIONARY_H_
+
+#include <fstream>
+#include <map>
+#include <string>
+#include <vector>
+#include <string.h>
+#include "MutexLock.h"
+#include "CreateDictionary.h"
+#include "Conf.h"
+/*
+ * 单例模式的类，将其构造函数定义为private， 只允许初始化一次
+ */
+class Dictionary {
+public:
+	virtual ~Dictionary();
+	static Dictionary *get_instance();  //定义返回该实例的一个对象指针
+	const std::map<std::string, std::size_t> get_map()const ;
+private:
+	Dictionary()
+    {
+		m_direction.clear();
+		Conf conf("dictionary");
+		std::string filename = conf.get_value("FileName") ;
+
+
+	   //创建服务器自己的词库
+		create_dictionary(filename, m_direction);
+    }
+
+	static MutexLock _lock;
+	static Dictionary *_p_dictionary;
+	std::map<std::string, std::size_t> m_direction ;  //服务器的字典词库
+
+	void create_dictionary(const std::string &filename, std::map<std::string, std::size_t> &direction);
+};
+
+#endif /* DICTIONARY_H_ */
---
+++ src/Dictionary.cpp
@@ -0,0 +1,54 @@
+/*
+ * Dictionary.cpp
+ *
+ *  Created on: May 6, 2014
+ *      Author: wang
+ */
+
+#include "Dictionary.h"
+
+MutexLock Dictionary::_lock;
+Dictionary *Dictionary::_p_dictionary = NULL ;
+
+Dictionary *Dictionary::get_instance()
+{
+	if(_p_dictionary == NULL)
+	{
+		_lock.lock();
+		_p_dictionary = new Dictionary;
+		_lock.unlock();
+	}
+	return _p_dictionary ;
+}
+//服务器创建自己的词库map,以供线程进行查询
+void Dictionary::create_dictionary(const std::string &filename, std::map<std::string, std::size_t> &direction)
+{
+	std::ifstream fin;
+	fin.open(filename.c_str());
+	if(!fin)
+	{
+		throw std::runtime_error("file open failed");
+	}
+	std::string word;
+	std::size_t count;
+	while(fin >> word >>count)   //构成词库
+	{
+		//统计单词的词频，去除重复词的存储
+		std::pair<std::map<std::string, std::size_t>::iterator, bool> ret =
+				m_direction.insert(std::make_pair(word, 1));
+		if (!ret.second)
+		{
+			++ret.first->second;
+		}
+	}
+}
+//返回词库
+const std::map<std::string, std::size_t> Dictionary::get_map()const
+{
+	return m_direction ;
+}
+Dictionary::~Dictionary() {
+	// TODO Auto-generated destructor stub
+	delete _p_dictionary ;
+}
+
