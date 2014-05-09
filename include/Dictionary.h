39eb00e856e3eb67f8506e8910a182478cff1347
xiangbai <wanglei8568@126.com>
2014-05-09 22:57:06 +0800

    为该类增添了建立索引方法

--- include/Dictionary.h
+++ include/Dictionary.h
@@ -15,31 +15,44 @@
 #include <string.h>
 #include "MutexLock.h"
 #include "CreateDictionary.h"
+#include "EncodingConverter.h"
+#include "StringUtil.h"
 #include "Conf.h"
 /*
  * 单例模式的类，将其构造函数定义为private， 只允许初始化一次
+ * 在初始化该类的同时就为字典建立一个索引，提高查询速度
  */
 class Dictionary {
 public:
 	virtual ~Dictionary();
 	static Dictionary *get_instance();  //定义返回该实例的一个对象指针
-	const std::map<std::string, std::size_t> &get_map() ;
+
+	std::vector<std::pair<std::string, std::size_t> > _word_vec ;  //采用下表直接可以得到相应词组及其词频
+
+	std::map<uint16_t, std::set<std::size_t> > m_word ;  //词，映射到该词所在的vector数组
+	std::map<uint16_t, std::set<std::size_t> >::iterator iter ;
 private:
 	Dictionary()
     {
-		m_direction.clear();
 		Conf conf("dictionary");
 		std::string filename = conf.get_value("FileName") ;
 
 	   //创建服务器自己的词库
-		create_dictionary(filename, m_direction);  //保存的有自己的词库，但这只保留的是英文词库
+		create_dictionary(filename);  //保存的有自己的词库，但这只保留的是英文词库
+		iter = m_word.begin();
     }
 
 	static MutexLock _lock;
 	static Dictionary *_p_dictionary;
-	std::map<std::string, std::size_t> m_direction ;  //服务器的字典词库
 
-	void create_dictionary(const std::string &filename, std::map<std::string, std::size_t> &direction);
+	//std::map<std::string, std::size_t> m_direction ;  //服务器的字典词库
+
+	void create_dictionary(const std::string &filename);
+	void create_index(std::string &word, std::size_t index);
+
+
+
+
 };
 
 #endif /* DICTIONARY_H_ */
--- src/Dictionary.cpp
+++ src/Dictionary.cpp
@@ -24,7 +24,7 @@ Dictionary *Dictionary::get_instance()
 	return _p_dictionary ;
 }
 //服务器创建自己的词库map,以供线程进行查询
-void Dictionary::create_dictionary(const std::string &filename, std::map<std::string, std::size_t> &direction)
+void Dictionary::create_dictionary(const std::string &filename)
 {
 	std::ifstream fin;
 	fin.open(filename.c_str());
@@ -34,22 +34,61 @@ void Dictionary::create_dictionary(const std::string &filename, std::map<std::st
 	}
 	std::string word;
 	std::size_t count;
+	std::pair<std::string, std::size_t> word_count ;
+	std::size_t index = 0 ;
+	std::size_t cur_index = 0 ;
 	while(fin >> word >>count)   //构成词库
 	{
 		//统计单词的词频，去除重复词的存储
-		std::pair<std::map<std::string, std::size_t>::iterator, bool> ret =
+		/*std::pair<std::map<std::string, std::size_t>::iterator, bool> ret =
 				m_direction.insert(std::make_pair(word, 1));
 		if (!ret.second)
 		{
 			++ret.first->second;
-		}
+		}*/
+		word_count = make_pair(word, count);
+		_word_vec.push_back(word_count);  //得到词，词频数组
+		cur_index = index++ ;
+		std::string temp = word_count.first ;
+		create_index(temp, cur_index);  // 建立索引
 	}
 }
-//返回词库
-const std::map<std::string, std::size_t> &Dictionary::get_map()
+//为每一个词建立一个索引
+void Dictionary::create_index(std::string &word, std::size_t index)
 {
-	return m_direction ;
+	EncodingConverter trans ;
+	StringUtil string_util ;
+	word = trans.utf8Togbk(word) ;
+	int i ;
+	unsigned char a ;
+
+	//读取每个字节
+	for(i = 0; i != word.size(); ++i)
+	{
+		a = word[i] ;
+		if(a & 0x80)  //结果是等于128，表示是汉字
+		{
+			unsigned char str_= (unsigned char)word[i] ;
+			unsigned char str_add = (unsigned char)word[++i] ;
+
+			uint16_t temp = str_<< 8 | str_add ;  //只是一个字节的uint16_t的值
+			//可能会有重复词的存在
+			iter->first = temp ;
+			iter->second.insert(index) ;
+
+		}
+		else  //结果小于128， 表示为ASCII
+		{
+			iter->first = (uint16_t)(unsigned char) word[i] ;
+			iter->second.insert(index) ;
+
+		}
+		++iter ;
+	}
+
 }
+//返回词库
+
 Dictionary::~Dictionary() {
 	// TODO Auto-generated destructor stub
 	delete _p_dictionary ;
