6a4ea73bfbc7b802f9de2e5f9ac362ad9a0bb402
xiangbai <wanglei8568@126.com>
2014-05-12 16:42:08 +0800

    增加cache查询数组成员变量，将工作线程中的cache查询类的指针传递进来

--- include/CacheThread.h
+++ include/CacheThread.h
@@ -8,14 +8,18 @@
 #ifndef CACHETHREAD_H_
 #define CACHETHREAD_H_
 #include "Thread.h"
-#include "CacheQuery.h"
+
+#include <vector>
+class CacheQuery ;
 class CacheThread:public Thread {
 public:
 	CacheThread();
 	virtual ~CacheThread();
 	void run() ; //继承线程的函数
+	void add_cache(CacheQuery *p_cache);  //通过接口来注册到线程中
 private:
 	void process();
+	std::vector<CacheQuery *> _p_cache_vec ;
 
 };
 
--- src/CacheThread.cpp
+++ src/CacheThread.cpp
@@ -6,8 +6,9 @@
  */
 
 #include "CacheThread.h"
+#include "CacheQuery.h"
 
-CacheThread::CacheThread() {
+CacheThread::CacheThread(){
 	// TODO Auto-generated constructor stub
 
 }
@@ -21,8 +22,17 @@ void CacheThread::run()
 }
 void CacheThread::process()
 {
-	CacheQuery cache_query ;
-	cache_query.write_into_cache() ;  //更新磁盘文件
+	//CacheQuery cache_query ;
+	//cache_query.write_into_cache() ;  //更新磁盘文件
+	for(std::vector<CacheQuery *>::iterator iter = _p_cache_vec.begin() ; iter != _p_cache_vec.end() ; ++iter)
+	{
+		CacheQuery *p_cache = *iter ;    //分别读取cache查询类，更新磁盘
+		p_cache->write_into_cache();
+	}
+}
+void CacheThread::add_cache(CacheQuery *p_cache)
+{
+	_p_cache_vec.push_back(p_cache) ;   //将工作线程中的cache注册到cache线程中
 }
 CacheThread::~CacheThread() {
 	// TODO Auto-generated destructor stub
